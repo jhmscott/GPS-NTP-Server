f4eb9b6f29efdc2304efc7f8cacb1bbd5e77245e
Justin Scott <justinhmscott@gmail.com>
2021-01-17 02:51:43 -0800

    Added global constants, CurrentTime mutex and some NtpPacket setters

--- ntpServer.py
+++ ntpServer.py
@@ -1,9 +1,16 @@
 import socket
 import struct
 import time
+import threading
 from enum import Enum
+import math
+import Queue
 
-GPS_POLL 
+GPS_POLL = 1.0              #1Hz GPS module is used
+CLK_PRECISION = 10 ** -9    #Perf Counter has Nanosecond precision
+NTP_VERSION = 3
+
+mutex = threading.Lock()
 
 class Mode(Enum):
     """NTP Mode Enum
@@ -29,7 +36,7 @@ class LeapInictaor(Enum):
     LAST_MINUTE_59 = 2
     ALARM = 3
 
-class currentTime:
+class CurrentTime:
     """Current Time Class
 
     Stores reference rime from an external source, along with the time
@@ -50,52 +57,106 @@ class currentTime:
         Parameters:
         time -- current time in UTC format
         """
+        mutex.acquire()
         self.__gpsTime = time
         self.__perfTime = time.perf_counter()
-
-    def getRefTime(self):
-        """Gets the reference time, the last updated time
-
-        Returns:
-        Reference time in UTC format
-        """
-        return self.__gpsTime
+        mutex.release()
+        
     
     def getTime(self):
         """Returns the current time, calculated from the reference 
-        and elapsed time
+        and elapsed time, as well as the reference time
 
         Returns:
-        Current Time in UTC format
+        Current Time in UTC format, 
+        Reference Time in UTC format
         """
-        elapsedTime = time.perf_counter - self.__perfTime
-        return (self.__gpsTime + elapsedTime)
+        mutex.acquire()
+        elapsedTime = self.__gpsTime + time.perf_counter - self.__perfTime
+        refTime = self.__gpsTime
+        mutex.release()
+
+        return (elapsedTime, refTime)
+        
 
 class NtpPacket: 
     """NTP Packet Class
 
-    Stores all the ntp packet fields
+    Stores packs and unpacks all the ntp packet fields
     """
 
     _PACKET_FORMAT = "!B B B b 11I"
 
-    def __init__(self, version, mode, txTimestamp):
-        self.leap = LeapInictaor.NO_WARNING
-        self.version = version
-        self.mode = mode
-        self.stratum = 1
-        
-        self.poll = 0
-        self.precision = 0
-        self.rootDelay = 0
-        self.rootDispersion = 0
-        self.refId = ""
+    def __init__(self, version, mode, refId):
+        """Constructor
 
-        self.refTimestamp = 0
-        self.originTimestamp = 0
-        self.rxTimestamp = 0
-        self.txTimestamp = txTimestamp
+        Initializes local Variables
+
+        Paramaters:
+        version -- the NTP version the packet is using (1,2,3,4)
+        mode -- the NTP mode (see Mode Enum)
+        refID -- For a Stratum 1 or primary server, this is a 4
+        character pneumonic that represents the time source
+
+        Code    External Reference Source
+        -------------------------------------------------------
+        LOCL    uncalibrated local clock 
+        CESM    calibrated Cesium clock
+        RBDM    calibrated Rubidium clock
+        PPS     calibrated quartz clock or other pulse-per-second source
+        IRIG    Inter-Range Instrumentation Group
+        ACTS    NIST telephone modem service
+        USNO    USNO telephone modem service<
+        PTB     PTB (Germany) telephone modem service
+        TDF     Allouis (France) Radio 164 kHz
+        DCF     Mainflingen (Germany) Radio 77.5 kHz
+        MSF     Rugby (UK) Radio 60 kHz
+        WWV     Ft. Collins (US) Radio 2.5, 5, 10, 15, 20 MHz
+        WWVB    Boulder (US) Radio 60 kHz
+        WWVH    Kauai Hawaii (US) Radio 2.5, 5, 10, 15 MHz 
+        CHU     Ottawa (Canada) Radio 3330, 7335, 14670 kHz 
+        LORC    LORAN-C radionavigation system
+        OMEG    OMEGA radionavigation system
+        GPS     Global Positioning Service
+        """
+        self.__leap = LeapInictaor.NO_WARNING
+        self.__version = version
+        self.__mode = mode
+        self.__stratum = 1
+        
+        self.__poll = 0
+        self.__precision = 0
+        self.__rootDelay = 0
+        self.__rootDispersion = 0
+        self.__refId = refId
+
+        self.__refTimestamp = 0
+        self.__originTimestamp = 0
+        self.__rxTimestamp = 0
+        self.__txTimestamp = 0
+
+    def setPoll(self, poll):
+        """Set Poll
+        
+        Sets Poll time for packet
+
+        Parameters:
+        poll -- poll time for packet
+        """
+        self.__poll = poll
     
+    def setPrecision(self, precisionFloat):
+        """Set Precision
+
+        Sets the precision of the server in the NTP Packet.
+        This is the base2 log of the actual precision, and is
+        stored as an 8 bit signed integer
+        
+        Parameters:
+        precisionFloat -- floating point value of the precision
+        """
+        self.__precision = round(math.log2(precisionFloat))
+
     def _floatToFixed(self, floatNum, fracBits):
         """Floating point to fixed point conversion
 
@@ -111,7 +172,7 @@ class NtpPacket:
         """
         intPart = int(floatNum)
         fracPart = int(abs(floatNum - int(floatNum)) * 2 ** fracBits)
-        return intPart << n | fracPart
+        return intPart << fracBits | fracPart
 
-    
+   
 
